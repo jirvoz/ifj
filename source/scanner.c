46b44110205ba30d1fc9ff6f509e49609042fcd9
tondo8881 <xnerec00@vutbr.cz>
2017-10-16 00:10:32 +0200

    finite automat is creating

--- source/scanner.c
+++ source/scanner.c
@@ -4,6 +4,8 @@ unsigned line = 1; 		//line counter
 
 int rest = -1;			//rest character
 
+char* tmp_string;
+
 char** key_words = {
 	"As", "Asc", "Declare", "Dim", "Do", "Double", "Else", "End", "Chr",
 	"Function", "If", "Input", "Integer", "Length", "Loop", "Print", "Return",
@@ -15,14 +17,168 @@ char** reserved_key_words = {
 	"Not", "Or", "Shared", "Static", "True"
 };
 
+//add character to 
+int addChar (char c, char* my_string) {
+
+	return SUCCESS;
+}
+
 int getNextToken(string *attr, int *type, FILE* source) {
 	string str = "";	//read string
-	unsigned state = 0;
+	int next = -1;
+	unsigned state = BEGIN;
 	char symbol;
 
 	do {
-		symbol = getchar;
+		symbol = getchar();
+		
+		if (state == START) {
+			if (isalpha(c) || c == '_') {
+				if (addChar(c, tmp_string)) {
+					state = IDENTIFIER;
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == '!') {
+				state = STRING;
+			}
+			else if (c == APOSTROPHE) {						
+				state = SINGLE-LINE-COMMENT;
+			}
+			else if (c == '/') {									
+				state = MULTI-LINE-COMMENT;
+			}
+			else if (isdigit(c)) {									
+				if (addChar(c, tmp_string)) {
+					state = NUMBER;
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == '+' || c == '-' || c == '*' || c == '/' || c == APOSTROPHE || c == '(' || c == ')') {								
+				if (addChar(c, tmp_string)) {
+					state = SINGLE-OPERATOR;
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == '<' || c == '>' || c == '=') {		
+				if (addChar(c, tmp_string)) {
+					state = MULTI-OPERATOR;
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c = EOF) {
+				//neco
+			}
+			else {
+				//error...
+			}
+		}
+		else if (state == NUMBER) {
 
+		}
+		else if (state == SINGLE-LINE-COMMENT) {
+			if (c == '\n') {	
+				state = BEGIN;
+			}
+			else if (c == EOF) {
+				//neco
+			}
+			//we don't store comments
+		}
+		else if (state == MULTI-LINE-COMMENT) {
+			if (c == 39) {
+				state = END-OF-COMMENT;
+			}
+			else if (c == EOF) {
+				//neco
+			}
+		}
+		else if (state == END-OF-COMMENT) {
+			if (c == '/') {
+				state = BEGIN;
+			}
+			else if (c == EOF) {
+				//neco
+			}
+			else {
+				state = MULTI-LINE-COMMENT;
+			}
+		}
+		else if (state == IDENTIFIER) {
+
+		}
+		else if (state == OPERATOR) {
+
+		}
+		else if (state == OPERATORS) {
+
+		}
+		else if (state == STRING) {
+
+		}
+		else if (state == NUMBER) {
+			if (isdigit(c)) {
+				if (addChar(c, tmp_string)) {
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == '.') {
+				if (addChar(c, tmp_string)) {
+					state = FLOATING-POINT;
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == 'e' || c == 'E') {
+				if (addChar(c, tmp_string)) {
+					state = EXPONENT;
+				}
+				else {
+					//error...
+				}
+			}
+			else {
+				next = c;
+				*type = INTEGER;
+				return OK;
+			}
+		}
+		else if (state == FLOATING-POINT) {
+			if (isdigit(c)) {
+				if (addChar(c, tmp_string)) {
+				}
+				else {
+					//error...
+				}
+			}
+			else if (c == 'e' || c == 'E') {
+				if (addChar(c, tmp_string)) {
+					state = EXPONENT;
+				}
+				else {
+					//error...
+				}
+			}
+			else {
+				next = c;
+				*type = FLOATING-POINT;
+				return OK;
+			}
+		}
+		else if (state == EXPONENT) {
+			
+		}
 	}
 
 }
--- source/scanner.h
+++ source/scanner.h
@@ -6,25 +6,33 @@
 
 enum tstates {
 	BEGIN,
-	MINUS,				//1 - komentar/minus
-	IN-COMMENT-CHECK, //2 - kontrola vnoreneho komentare 
-	CHECK 			//3 - kontrola druheho znaku vnoreneho komentare
-	COMMENT			//4 - komentar
-	IN-COMMENT,	// 5 - vnoreny komentar
-	END-IN-COMMENT, 	//6 - konec vnoreneho komentare
+	MINUS,				//1 - minus
+	//IN-COMMENT-CHECK, //2 - kontrola vnoreneho komentare 
+	//CHECK 			//3 - kontrola druheho znaku vnoreneho komentare
+	SINGLE-LINE-COMMENT		//4 - comment on 1 line
+	MULTI-LINE-COMMENT	//5 - multi line comment
+	END-OF-COMMENT
+	//END-IN-COMMENT, 	//6 - konec vnoreneho komentare
 	IDENTIFIER,		//7 - identifikator
 	STRING,				//8 - retezec
 	ESCAPE-SEQUENCE,	//9 - kontrola escape sekvenci
+	ZERO-NUMBER			//delete first ZEROS in the number
 	NUMBER, 				//10 - vynechani pocatecnich nul u cisla
-	INT-PART,	//11 - celociselna cast cisla
+	EXPONENT,	//11 - celociselna cast cisla
 	UNEMPTY-CHECK, //12 - kontrola neprazdnosti desetineho cisla
-	FLOAT-NUMBER,	//13 - desetine cislo
+	FLOATING-POINT,	//13 - desetine cislo
 	POWER-CHECK,	//14 - kontrola neprazdnosti mocniny nebo zadani +- u mocniny
 	PLUSMINUS-CHECK,			//15 - kontrola neprazdnosti cisla mocniny po zadani +-
 	ZERO-CHECK,		//16 - vynechani pocatecnich nul u mocniny
 	POWER-VALUE,				//17 - hodnoty mocniny
 	CONCATENATION,		//18 - konkatenace
-	OPERATORS,//19 - rozliseni dvouznakovych operatoru od jednoznakovych == <= >= 
-	TWO-CHAR-OPERATOR ,//20 - kontrola spravnosti dvouznakoveho operatoru ~=
+	SINGLE-OPERATOR,//19 - rozliseni dvouznakovych operatoru od jednoznakovych == <= >= 
+	MULTI-OPERATOR ,//20 - kontrola spravnosti dvouznakoveho operatoru ~=
 }
+
+#define SUCCESS 1
+#define FAILURE 0
+#define APOSTROPHE 39
+#define BACKSLASH 92
+
 
