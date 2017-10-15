ab43d27e59b32e51185719154cf2b100db26abf1
tondo8881 <xnerec00@vutbr.cz>
2017-10-15 15:49:41 +0200

    first minor things added

--- source/scanner.c
+++ source/scanner.c
@@ -1,2 +1,28 @@
 #include "scanner.h"
 
+unsigned line = 1; 		//line counter
+
+int rest = -1;			//rest character
+
+char** key_words = {
+	"As", "Asc", "Declare", "Dim", "Do", "Double", "Else", "End", "Chr",
+	"Function", "If", "Input", "Integer", "Length", "Loop", "Print", "Return",
+	"Scope", "String", "SubStr", "Then", "While"
+};
+
+char** reserved_key_words = {
+	"And", "Boolean", "Continue", "Elseif", "Exit", "False", "For", "Next",
+	"Not", "Or", "Shared", "Static", "True"
+};
+
+int getNextToken(string *attr, int *type, FILE* source) {
+	string str = "";	//read string
+	unsigned state = 0;
+	char symbol;
+
+	do {
+		symbol = getchar;
+
+	}
+
+}
--- source/scanner.h
+++ source/scanner.h
@@ -3,3 +3,28 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <string.h>
+
+enum tstates {
+	BEGIN,
+	MINUS,				//1 - komentar/minus
+	IN-COMMENT-CHECK, //2 - kontrola vnoreneho komentare 
+	CHECK 			//3 - kontrola druheho znaku vnoreneho komentare
+	COMMENT			//4 - komentar
+	IN-COMMENT,	// 5 - vnoreny komentar
+	END-IN-COMMENT, 	//6 - konec vnoreneho komentare
+	IDENTIFIER,		//7 - identifikator
+	STRING,				//8 - retezec
+	ESCAPE-SEQUENCE,	//9 - kontrola escape sekvenci
+	NUMBER, 				//10 - vynechani pocatecnich nul u cisla
+	INT-PART,	//11 - celociselna cast cisla
+	UNEMPTY-CHECK, //12 - kontrola neprazdnosti desetineho cisla
+	FLOAT-NUMBER,	//13 - desetine cislo
+	POWER-CHECK,	//14 - kontrola neprazdnosti mocniny nebo zadani +- u mocniny
+	PLUSMINUS-CHECK,			//15 - kontrola neprazdnosti cisla mocniny po zadani +-
+	ZERO-CHECK,		//16 - vynechani pocatecnich nul u mocniny
+	POWER-VALUE,				//17 - hodnoty mocniny
+	CONCATENATION,		//18 - konkatenace
+	OPERATORS,//19 - rozliseni dvouznakovych operatoru od jednoznakovych == <= >= 
+	TWO-CHAR-OPERATOR ,//20 - kontrola spravnosti dvouznakoveho operatoru ~=
+}
+
