ffe1e6aeabba4dffab212c6bc09c6e4f02ba9a9c
Jiří Vozár <jiri.vozar@gmail.com>
2017-10-30 14:33:39 +0100

    Add dummy main.c functionality
    Program main.c now calls getNextToken and writes it to stdout.

--- source/main.c
+++ source/main.c
@@ -1,4 +1,31 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "scanner.h"
+
+int main ()
+{
+    token tok;
+    do
+    {
+        if (getNextToken(&tok, stdin) != OK)
+            return 1;
+
+        switch (tok.type)
+        {
+            case IDENTIFIER:
+            case STRING_TOK:
+                printf("%d %s\n", tok.type, tok.attribute.identifier_string->str);
+                break;
+            case INTEGER_TOK:
+                printf("%d %ld\n", tok.type, tok.attribute.number);
+                break;
+            default:
+                printf("%d\n", tok.type);
+                break;
+        }
+    }
+    while (tok.type != END_OF_FILE);
+
+    return 0;
 }
