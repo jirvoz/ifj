8187d4af1c39dd498b6986ec7fbe2816bafb99e2
tondo8881 <xnerec00@vutbr.cz>
2017-11-04 13:07:26 +0100

    error strings modified, error_count++

--- src/errors.c
+++ src/errors.c
@@ -6,10 +6,10 @@ unsigned error_count = 0;
 char* err_message[] = {
     "Nastala chyba v programu v rámci lexikální analýzy\n",
     "Nastala chyba v programu v rámci syntaktické analýzy\n",      
-    "Nastala sémantická chyba v programu – nedefinovaná funkce/promenná, pokus o redefinici funkce/promenné, atd\n",
-    "Nastala sémantická chyba typové kompatibility v aritmetických,retezcových a relacních výrazech, príp. špatný pocet ci typ parametru u volání funkce\n",
+    "Nastala sémantická chyba v programu\n",
+    "Nastala sémantická chyba typové kompatibility\n",
     "Nastala jiná sémantická chyba\n",
-    "Nastala chyba při alokaci paměti\n"
+    "Nastala vnitřní chyba\n"
 }; 
 
 void addError (unsigned line, err_code code) {
@@ -24,5 +24,6 @@ void addError (unsigned line, err_code code) {
         fprintf(stderr, err_message[code - 1]);
     }
     fprintf(stderr, "Řádek: %u\n", line); 
+    error_count++;
 }
 
