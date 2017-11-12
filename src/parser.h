5cad100a5c5a4e8b2100b9b27118656dec3d06ed
Jiří Vozár <jiri.vozar@gmail.com>
2017-11-13 00:22:13 +0100

    Update and comment parser and statements .h file

--- src/parser.h
+++ src/parser.h
@@ -4,14 +4,28 @@
 #include <stdbool.h>
 #include "scanner.h"
 
+// Shortcut for getting next token
 #define UPDATE_LAST_TOKEN(); if (getNextToken(&last_token, stdin) != SUCCESS) return false;
 
+
+// Last loaded token for public use
 extern tToken last_token;
 
+
+// Temporary function that eats tokens till EOL, END, EOF
 bool skip_statement();
 
+// Crossroads for parsing single statements
+// (variable definition, assingnment, if, while,...)
+bool statement();
+
+// Parsing of set of statements ended by END token
 bool statement_list();
 
+// Parsing crossroads for functions and main scope
+bool program();
+
+// Main function for eating IFJ17 tokens and forging IFJcode17
 bool parse();
 
 #endif
