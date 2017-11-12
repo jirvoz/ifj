007c539aacc9d892ed38288e7bc23490fc8d0da9
tondo8881 <xnerec00@vutbr.cz>
2017-11-12 23:58:39 +0100

    stack for tokens, basic functionality

---
+++ src/stack.h
@@ -0,0 +1,24 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stdbool.h>
+#include "scanner.h"
+
+    
+//structure of Stack with dynamic size
+typedef struct tStack 
+{
+    tToken* arr;        //data
+    int size;           //actual size of stack
+    int top;            //index of item on the top of stack
+} tStack;
+
+//declarations of functions
+
+tStack* stackInit();
+bool stackEmpty(tStack* stack);
+void stackPush(tStack* stack, tToken* token);
+void stackPop(tStack* stack);
+void stackTop(tStack* stack, tToken* token);
+void stackFree(tStack* stack);
+#endif
