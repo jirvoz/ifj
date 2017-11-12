4102440d753992a8c3e5db8f83863d1c985bd289
tondo8881 <xnerec00@vutbr.cz>
2017-11-13 00:18:16 +0100

    new line bracelets

--- src/symtable.c
+++ src/symtable.c
@@ -6,34 +6,40 @@ int HTSIZE = MAX_HTSIZE;
 int solved;
 
 //hash function
-
-int hashCode (char* key) {
+int hashCode (char* key) 
+{
     int retval = 0;
     int keylen = strlen(key);
-    for ( int i=0; i<keylen; i++ )
+    for (int i=0; i<keylen; i++)
         retval += key[i];
     return ( retval % HTSIZE );
 }
 
 //initializaton of hash table before first use
-void htInit (tHTable* ptrht) {
-    for (int i = 0; i < MAX_HTSIZE; i++) {
+void htInit (tHTable* ptrht) 
+{
+    for (int i = 0; i < MAX_HTSIZE; i++) 
+    {
         (*ptrht)[i] = NULL; 
     }
 }
 
 //this function set data of hash table item
-void setSymbol (symbol_type type, tData data, tHTItem* ptr) {
+void setSymbol (symbol_type type, tData data, tHTItem* ptr) 
+{
     ptr->type = type;
     ptr->data = data;
 }
 
 //searching function
-tHTItem* htSearch ( tHTable* ptrht, char* key ) {
+tHTItem* htSearch ( tHTable* ptrht, char* key ) 
+{
     tHTItem* ptr = (*ptrht)[hashCode(key)];
 
-    while (ptr != NULL) {
-        if (!strcmp(ptr->key, key)) {
+    while (ptr != NULL) 
+    {
+        if (!strcmp(ptr->key, key)) 
+        {
             return ptr;
         }
         ptr = ptr->ptrnext;
@@ -42,10 +48,12 @@ tHTItem* htSearch ( tHTable* ptrht, char* key ) {
 }
 
 //function insert item in the table
-void htInsert (tHTable* ptrht, char* key, symbol_type type, tData data) {
+void htInsert (tHTable* ptrht, char* key, symbol_type type, tData data) 
+{
     tHTItem* ptr = NULL;
 
-    if ((ptr = htSearch(ptrht, key)) != NULL) {
+    if ((ptr = htSearch(ptrht, key)) != NULL) 
+    {
         setData(type, data, ptr);
     }
     else {
@@ -59,31 +67,39 @@ void htInsert (tHTable* ptrht, char* key, symbol_type type, tData data) {
 }
 
 //function returns data of item 
-tData* htRead (tHTable* ptrht, char* key) {
+tData* htRead (tHTable* ptrht, char* key) 
+{
     tHTItem* ptr;
-    if ((ptr = htSearch(ptrht, key)) != NULL) {
+    if ((ptr = htSearch(ptrht, key)) != NULL) 
+    {
         return &(ptr->data);
     }
-    else {
+    else 
+    {
         return NULL;
     }
 }
 
 //delete item from table
-void htDelete (tHTable* ptrht, char* key) {
+void htDelete (tHTable* ptrht, char* key) 
+{
     int i = hashCode(key);
     tHTItem* ptr = (*ptrht)[i];
     tHTItem* tmp;
 
-    if (ptr != NULL) {
-        if (!strcmp(ptr->key, key)) {
+    if (ptr != NULL) 
+    {
+        if (!strcmp(ptr->key, key)) 
+        {
             tmp = ptr->ptrnext;
             free(ptr);
             (*ptrht)[i] = tmp;
             return;
         }
-        while (ptr->ptrnext != NULL) {
-            if (ptr->ptrnext->key == key) {
+        while (ptr->ptrnext != NULL) 
+        {
+            if (ptr->ptrnext->key == key) 
+            {
                 tmp = ptr->ptrnext;
                 ptr->ptrnext = ptr->ptrnext->ptrnext;
                 free(tmp);
@@ -95,12 +111,15 @@ void htDelete (tHTable* ptrht, char* key) {
 }
 
 //clear whole hash table
-void htClearAll (tHTable* ptrht) {
+void htClearAll (tHTable* ptrht) 
+{
     tHTItem* ptr;
-    for (int i = 0; i < MAX_HTSIZE; i++) {
-        if ((*ptrht)[i] != NULL) {
-
-            while ((*ptrht)[i] != NULL) {
+    for (int i = 0; i < MAX_HTSIZE; i++) 
+    {
+        if ((*ptrht)[i] != NULL) 
+        {
+            while ((*ptrht)[i] != NULL) 
+            {
                 ptr = (*ptrht)[i];
                 (*ptrht)[i] = ptr->ptrnext;
                 free(ptr);
