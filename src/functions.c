e6cd4c48e455d1b770161495c58eac126bc07b77
Jiří Vozár <jiri.vozar@gmail.com>
2017-11-15 12:33:49 +0100

    Add reading function name

--- src/functions.c
+++ src/functions.c
@@ -3,9 +3,56 @@
 #include "functions.h"
 #include "parser.h"
 #include "statements.h"
+#include "symtable.h"
 
 bool call(char* name)
 {
+    // TODO search in symtable, evaluate params and call
+    printf("CALL $%s\n", name);
+    return true;
+}
+
+bool function_params(tSymbol symbol)
+{
+    return true;
+}
+
+bool function_header(bool define)
+{
+    // last_token.type is FUNCTION
+
+    UPDATE_LAST_TOKEN();
+
+    // Check for identifier token
+    if (last_token.type != IDENTIFIER_TOK)
+        ERROR_AND_RETURN(SYN_ERROR, "Expected identifier after DIM.");
+
+    char * identif_name = last_token.attribute.string_ptr;
+
+    // Check symtable if variable exists
+    tSymbol* symbol = htSearch(func_table, last_token.attribute.string_ptr);
+    
+    // Handle multiple declaration
+    if (!define && symbol)
+        ERROR_AND_RETURN(SEM_PROG_ERROR, "Function '%s' is already declared.",
+            last_token.attribute.string_ptr);
+    
+    // Handle multiple definitions
+    if (define && symbol && symbol->defined)
+        ERROR_AND_RETURN(SEM_PROG_ERROR, "Function '%s' is already defined.",
+            last_token.attribute.string_ptr);
+
+    printf("LABEL $%s\n", identif_name);
+    
+    // TODO: read params and return value
+
+    if (!symbol)
+        htInsert(func_table, identif_name, (tSymbol){ .type=UNDEFINED_TOK, .defined=define });
+
+    if (!skip_statement())
+        return false;
+
+    printf("RETURN\n");
     return true;
 }
 
@@ -13,24 +60,22 @@ bool function_decl()
 {
     // last_token.type is DECLARE
 
-    if (!skip_statement())
-        return false;
-    return true;
+    UPDATE_LAST_TOKEN();
+    
+    if (last_token.type != FUNCTION)
+        ERROR_AND_RETURN(SYN_ERROR, "Expected FUNCTION after DECLARE.");
+
+    return function_header(false);
 }
 
 bool function_def()
 {
     // last_token.type is FUNCTION
 
-    UPDATE_LAST_TOKEN();
-    
-    if (last_token.type != IDENTIFIER_TOK)
+    // Parse function header (name, parameters and return value)
+    if (!function_header(true))
         return false;
-    
-    printf("LABEL $%s\n", last_token.attribute.string_ptr);
-
-    if (!skip_statement())
-        return false;
+    // last_token.type is EOL_TOK
 
     // parse the inside of function
     if (!statement_list())
