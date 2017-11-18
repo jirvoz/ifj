3b798de0a0abd48902ec90d17934eff00c38179f
tondo8881 <xnerec00@vutbr.cz>
2017-11-18 23:25:41 +0100

    list function moved to expression

--- src/expressions.c
+++ src/expressions.c
@@ -1,10 +1,98 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "errors.h"
 #include "expressions.h"
 #include "functions.h"
 #include "parser.h"
-#include "postfix_list.h"
 #include "statements.h"
+
+/*****************************LIST-STUFF**************************/
+
+//list initialization
+void listInit(tList* list)
+{
+    list->first  = NULL;
+    list->last   = NULL;
+    list->active = NULL;
+}
+  
+//list dealocation
+void listFree(tList* list)
+{
+    tList_item* ptr;
+    while (list->first != NULL)
+    {
+        ptr = list->first;
+        list->first = list->first->next;
+
+        //free whole item
+        free(ptr);
+    }
+}
+
+//insert new item to end of the list
+void listInsertLast(tList* list, tToken token, p_table_index index)
+{
+    tList_item *new_item;
+    new_item = malloc(sizeof (tList_item));
+    new_item->token = token;
+    new_item->index = index;
+    new_item->next = NULL;
+
+    if (list->first == NULL)
+    {
+        list->first = new_item;
+    }
+    else
+    {
+        list->last->next = new_item;
+    }
+    list->last = new_item;
+}
+
+//activate first item
+void listFirst(tList* list)
+{
+    list->active = list->first;
+}
+
+//activate item after actual activated item
+void listNext(tList* list)
+{
+    if (list->active != NULL)
+    {
+        list->active = list->active->next;   
+    }
+}
+
+//activate token on pointer
+void listGoto(tList* list, tList_item* item)
+{
+    list->active = item;
+}
+
+//return pointer of last item 
+tList_item* listGetPointerLast(tList* list)
+{
+    return list->last;
+}
+
+//return active item
+tList_item* listGetData(tList* list)
+{
+    if (list->active == NULL)
+    {
+        addError(OTHER_ERROR, "Internal error");
+        return NULL;
+    }
+    else
+    {
+        return list->active;  
+    } 
+}
+
+/*************************END-OF-LIST-STUFF******************************/
+
 
 //size of precedence table
 #define P_TAB_SIZE 19
@@ -36,7 +124,7 @@ const int precedence_table[P_TAB_SIZE][P_TAB_SIZE] =
 //number of rules
 #define RULE_COUNT 10
 
-
+/*
 //this function compare two rules
 bool compareRules(int* rule1, int* rule2)
 {
@@ -67,7 +155,7 @@ int generateRule(int *rule)
 
 //declarations of variable in symboltable
 void 
-
+*/
 bool getTerm(tToken token, p_table_index* index)
 {
     //token is identifier, figure out if var or func
@@ -122,17 +210,18 @@ bool getTerm(tToken token, p_table_index* index)
 
 bool expression(token_type expected_type)
 {
-	tList list_infix = getInfix(expected_type);
-
-    //return true;
+	tList* list_infix = getInfix(expected_type);
+    //will be deleted, just for compilation
+    list_infix = (void*) list_infix;
+    return true;
 }
 
-tList getInfix(token_type expected_type) 
+tList* getInfix(token_type expected_type) 
 {	
 	tToken token;
 	getNextToken(&token, stdin);
 
-	tList list_infix;
+	tList* list_infix = malloc(sizeof(tList));
 
 	p_table_index index;
 
@@ -143,50 +232,50 @@ tList getInfix(token_type expected_type)
 		case INTEGER_TOK:					// expected token is INTEGER or DOUBLE 
 		case FLOATING_POINT_TOK:
 		{
-			listInit(&list_infix);
+			listInit(list_infix);
 
-			while (getTerm(token, &index) && index != DOLAR_IN) ;
+			while (getTerm(token, &index) && index != DOLAR_IN)
 			{
 				if(index == INT_IN || index == DOUBLE_IN || index == PLUS_IN || index == MINUS_IN || index == MUL_IN || 	//Operands and operations allowed when expected type is INTEGER or DOUBLE
 				   index == FLOAT_DIV_IN || index == INT_DIV_IN || index == LEFT_PARENT_IN || index == RIGHT_PARENT_IN)
 				{
-    				listInsertLast(&list_infix, token);
+    				listInsertLast(list_infix, token, index);
     				getNextToken(&token, stdin);		
 				}
 				else
 				{
 					addError(SEM_TYPE_ERROR,"Bad operation or operand in expression");
-					listFree(&list_infix);
+					listFree(list_infix);
 				}
 			}
 
 			if(index != DOUBLE_IN)
 			{
-				listFree(&list_infix);
+				listFree(list_infix);
 			}
 
 			return list_infix; 
 		}
 		case STRING_TOK:
 		{
-			while (getTerm(token, &index) && index != DOLAR_IN);
+			while (getTerm(token, &index) && index != DOLAR_IN)
 			{
 				if(index == STRING_IN || index == PLUS_IN || index == DOLAR_IN)
 				{
-					listInsertLast(&list_infix, token);
+					listInsertLast(list_infix, token, index);
 					//concatenate
     				getNextToken(&token, stdin);		
 				}
 				else
 				{
 					addError(SEM_TYPE_ERROR,"Bad operation or operand in expression");
-					listFree(&list_infix);
+					listFree(list_infix);
 				}
 			}
 
 			if (index != DOLAR_IN)
 			{
-				listFree(&list_infix);
+				listFree(list_infix);
 			}
 
 			return list_infix; 
@@ -194,19 +283,23 @@ tList getInfix(token_type expected_type)
 
 		case BOOLEAN:
 		{
-			while (getTerm(token, &index) && index != DOLAR_IN);
+			while (getTerm(token, &index) && index != DOLAR_IN)
 			{
-				listInsertLast(&list_infix, token);
+				listInsertLast(list_infix, token, index);
     			getNextToken(&token, stdin);		
 			}
 
 			if (index != DOLAR_IN)
 			{
-				listFree(&list_infix);
+				listFree(list_infix);
 			}
 
 			return list_infix; 
-		}	
+		}
+        default:
+            listFree(list_infix);
+            addError(OTHER_ERROR, "Unknown error");
+            return list_infix;
 	}	
 }
 
--- src/expressions.h
+++ src/expressions.h
@@ -28,10 +28,40 @@ typedef enum p_table_index
     DOLAR_IN
 } p_table_index;
 
+/***********************LIST structures*****************************/
+
+typedef struct tList_item 
+{
+    tToken token;
+    p_table_index index;
+    struct tList_item* next;
+} tList_item;
+    
+typedef struct tList
+{
+    tList_item* first;       //pointer to first item
+    tList_item* last;        //pointer to last item
+    tList_item* active;      //pointer to active item
+} tList;
+
 // Parsing of expressions
 // expected_type - type of variable, that should end on the top of stack
 //                 can be INTEGER, DOUBLE, STRING, BOOL
 //                 or UNDEFINED_TOK = 0 (no specific type needed)
 bool expression(token_type expected_type);
+
+tList* getInfix(token_type expected_type);
+bool getTerm(tToken token, p_table_index* index);
+
+/*********************LIST functions*******************************/
+
+void listInit(tList* list);
+void listFree(tList* list);
+void listInsertLast(tList* list, tToken token, p_table_index index);
+void listFirst(tList* list);
+void listNext(tList* list);
+void listGoTo(tList* list, tList_item* token);
+tList_item* listGetPointerLast(tList* list);
+tList_item* listGetData(tList* list);
 
 #endif
