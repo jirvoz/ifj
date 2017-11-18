dd343722c6d3293ba73c431d28ce4957c6bbfa79
tondo8881 <xnerec00@vutbr.cz>
2017-11-18 11:32:37 +0100

    enums for precedence table added

--- src/expressions.c
+++ src/expressions.c
@@ -5,6 +5,29 @@
 #include "parser.h"
 #include "statements.h"
 
+//rows and cols in precedence table
+typedef enum p_table_index
+{
+	EQ_EXPR_IN,
+	NOT_EQ_IN,
+	LESS_EQ_IN,
+	MORE_EQ_IN,
+	LESS_IN,
+	PLUS_IN,
+	MINUS_IN,
+	MUL_IN,
+	FLOAT_DIV_IN,
+	INT_DIV_IN,
+	EQ_COMMAND_IN,
+	UNARY_MINUS_IN,
+	INT_IN,
+	DOUBLE_IN,
+	STRING_IN,
+	LEFT_PARENT_IN,
+	RIGHT_PARENT_IN,
+	DOLAR_IN
+}
+
 bool expression(token_type expected_type)
 {
     return true;
