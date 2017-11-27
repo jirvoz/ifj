0772b299ddc5d5e4f3714906042afb1d60ccc6d9
tondo8881 <xnerec00@vutbr.cz>
2017-11-27 19:19:10 +0100

    strings refactorized

--- src/strings.c
+++ src/strings.c
@@ -8,7 +8,7 @@
 
 int stringInit(string* str_1)   //function initialize new str
 {
-    str_1->str = (char*) malloc(STR_LEN_INC);
+    str_1->str = malloc(STR_LEN_INC);
 
     if (str_1->str != NULL)
     {
@@ -18,7 +18,6 @@ int stringInit(string* str_1)   //function initialize new str
        
         return true;
     }
-
     else
        return false;
 }
@@ -39,15 +38,11 @@ int stringAddChar(char c, string *str_1) //function to add new char to the end o
     if (str_1->length + 1 >= str_1->allocatedSize)
     {
         str_1->str = (char*) realloc(str_1->str, (str_1->length + STR_LEN_INC));
+
         if (str_1->str != NULL)
-        {
             str_1->allocatedSize = str_1->length + STR_LEN_INC;
-        }
         else
-        {
-            return false;  
-        }
-              
+            return false;
     }
     
     str_1->str[str_1->length] = c;
@@ -65,16 +60,12 @@ int stringConcat(const char* str_2, string* str_1)
         str_1->str = (char*) realloc(str_1->str, (str_1->length + str_2_length + STR_LEN_INC));
 
         if (str_1->str != NULL)
-        {
             str_1->allocatedSize = str_1->length + str_2_length + STR_LEN_INC;
-        }
         else
-        {
             return false;
-        }
     }
+
     strcat(str_1->str, str_2);
     str_1->length = str_1->length + str_2_length;
-
     return true;
 }
