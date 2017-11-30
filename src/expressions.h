ea7637f24dbfafd812c4ed99ec3cea66bfa051c7
tondo8881 <xnerec00@vutbr.cz>
2017-11-30 16:59:17 +0100

    stack.h is back

--- src/expressions.h
+++ src/expressions.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 #include "scanner.h"
-//#include "stack.h"
+#include "stack.h"
 
 //rows and cols in precedence table
 typedef enum p_table_index
@@ -29,21 +29,13 @@ typedef enum p_table_index
     STRING_IN
 } p_table_index;
 
-//structure sent to generateInstruction
-//data of stack
+// Structure sent to generateInstruction
+// Data of stack
 typedef struct tTerm
 {
-    tToken token;
     enum p_table_index index;
+    tToken token;
 } tTerm;
-
-//structure of Stack with dynamic size
-typedef struct tStack 
-{
-    struct tTerm* arr;        //data
-    int size;           //actual size of stack
-    int top;            //index of item on the top of stack
-} tStack;
 
 // Parsing of expressions
 // expected_type - type of variable, that should end on the top of stack
@@ -51,23 +43,11 @@ typedef struct tStack
 //                 or UNDEFINED_TOK = 0 (no specific type needed)
 bool expression(token_type expected_type);
 
-//other functions
+//Other functions
 bool postfix(token_type expected_type, token_type return_type, struct tTerm* term, struct tStack* stack);
 bool getTerm(tTerm* term);
 bool getPriority (tTerm* term, tStack* stack, token_type return_type);
 bool generateInstruction(token_type return_type, tTerm sent_term);
 void memoryClear(tTerm* term, tStack* stack);
-
-//just for tests
-//void printTerm(tTerm term);
-
-
-//stack functions
-tStack* stackInit();
-bool stackEmpty(tStack* stack);
-void stackPush(tStack* stack, struct tTerm term);
-struct tTerm* stackPop(tStack* stack);
-struct tTerm* stackTop(tStack* stack);
-void stackFree(tStack* stack);
 
 #endif
